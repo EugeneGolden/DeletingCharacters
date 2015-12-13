5ec37f1ef015c93b93d3277b114f3cea3fbb5416
Eugene <yeug@gmail.com>
2015-12-13 13:02:58 +0200

    first

---
+++ DeletingCharacters/StringProcessing.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace DeletingCharacters
+{
+    class StringProcessing
+    {
+        //Написать метод, входными параметрами которого являются две строки.
+        //Метод должен в первой заданной строке удалить каждый символ, принадлежащий и второй заданной строке.
+        public string DeleteCharacter(string firstString, string secondString)
+        {
+            //Конвертируем строку в массивы char
+            char[] firstStringToArray = firstString.ToCharArray();
+            char[] secondStringToArray = secondString.ToCharArray();
+
+            //Создадим два объекта типа ArrayList для обработки ранее созданных массивов char
+            ArrayList firstArrayList = new ArrayList();
+            ArrayList secondArrayList = new ArrayList();
+
+            //Добавим элменты массивов char в ArrayList объекты
+            firstArrayList.AddRange(firstStringToArray);
+            secondArrayList.AddRange(secondStringToArray);
+
+
+            for (int i = 0; i < firstStringToArray.Length; i++)
+            {
+                for (int j = 0; j < secondStringToArray.Length; j++)
+                {
+                    if (firstStringToArray[i] == secondStringToArray[j])
+                    {
+                        firstArrayList.Remove(firstStringToArray[i]);
+                    }
+                }
+            }
+
+            string outputFirstString = null;
+            foreach (var str in firstArrayList)
+            {
+                outputFirstString += str.ToString();
+            }
+            return outputFirstString;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первую строку: ");
+            string firstString = Console.ReadLine();
+            Console.Write("Введите вторую строку: ");
+            string secondString = Console.ReadLine();
+
+            StringProcessing searchDelete = new StringProcessing();
+            string outputString = searchDelete.DeleteCharacter(firstString, secondString);
+            Console.WriteLine("Результат после анализа: " + outputString);
+            Console.ReadKey();
+        }
+    }
+}
